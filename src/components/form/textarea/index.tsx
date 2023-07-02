a589a679527d4a0d3db4c8b2b483d9de4ab88850
Igor Stefano <igstefano90@live.com>
2023-07-02 19:31:01 -0300

    feat(textarea): Cria novo componente de textarea para compor formulários

---
+++ src/components/form/textarea/index.tsx
@@ -0,0 +1,39 @@
+import type { ComponentPropsWithoutRef } from "react";
+import { useState } from "react";
+
+interface TextAreaProps extends ComponentPropsWithoutRef<"textarea"> {
+  name: string;
+  label: string;
+  required?: boolean;
+  disabled?: boolean;
+  placeholder?: string;
+}
+
+export default function TextArea({
+  label,
+  name,
+  required,
+  disabled,
+  placeholder,
+  ...rest
+}: TextAreaProps) {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="flex w-full flex-col">
+      <label className="pb-1 text-xs text-burgundy" htmlFor={name}>
+        {label} {required && " *"}
+      </label>
+      <textarea
+        className="h-32 w-full resize-none rounded border border-gray-50 bg-lightYellow p-2 text-xs transition-all duration-300 focus-visible:border-solid focus-visible:border-burgundy disabled:bg-gray-200 disabled:text-gray-900 disabled:opacity-80"
+        id={name}
+        name={name}
+        disabled={disabled}
+        required={required}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={placeholder}
+        value={value}
+      />
+    </div>
+  );
+}
