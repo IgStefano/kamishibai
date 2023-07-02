11042614b605e05b532943ed4d5bb5d15bf3e5ab
Igor Stefano <igstefano90@live.com>
2023-07-02 19:26:25 -0300

    feat(quests): Adiciona rota para visualizar quests de uma determinada campanha

--- src/pages/campaigns/[id]/index.tsx
+++ src/pages/campaigns/[id]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import QuestListItem from "@components/campaigns/quest-list-item";
 import type { CheckboxT } from "@components/form/checkbox/checkbox-wrapper";
 import CheckboxWrapper from "@components/form/checkbox/checkbox-wrapper";
@@ -8,12 +9,47 @@ import type { IncomingMessage, ServerResponse } from "http";
 import { useEffect, useState } from "react";
 import { getServerAuthSession } from "../../../server/auth";
 import { AnimatePresence, motion } from "framer-motion";
+import { api } from "@utils/api";
+import { useRouter } from "next/router";
+import TextArea from "@components/form/textarea";
 
 export default function CampaignQuests() {
+  const router = useRouter();
+  const campaign = api.campaign.getCampaignById.useQuery({
+    id: router.query.id as string,
+  })?.data;
+  const quests = api.quest.getQuests.useQuery({}).data;
+  const mutation = api.quest.newQuest.useMutation();
+
   const [isOpen, setIsOpen] = useState(false);
   const [openQuests, setOpenQuests] = useState<string[]>([]);
   const [activities, setActivities] = useState<string[]>([]);
   const [checkboxes, setCheckboxes] = useState<CheckboxT[]>([]);
+
+  const handleCreateQuest = () => {
+    const formData = new FormData(
+      document.getElementById("new-quest") as HTMLFormElement
+    );
+
+    if (campaign)
+      mutation.mutate({
+        questName: formData.get("questName")!.toString(),
+        activities: formData
+          .get("activities")!
+          .toString()
+          .split(",")
+          .map((activity) => {
+            return { activityName: activity, activityStatus: "not_started" };
+          }),
+        campaignId: campaign.id,
+        description: formData.get("description")!.toString(),
+        isVisible: true,
+        recommendedLevel: Number(formData.get("recommendedLevel")!.toString()),
+        reward: formData.get("reward")!.toString(),
+      });
+
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     setCheckboxes(
@@ -33,32 +69,48 @@ export default function CampaignQuests() {
       isLogged
       addIcon
       message="Crie agora uma aventura para esta campanha!"
-      subHeading="Hypernatural"
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      subHeading={campaign?.name}
       setIsModalOpen={setIsOpen}
     >
       <ul className="my-4 flex list-none flex-col gap-4">
-        <QuestListItem
-          id="1"
-          openQuests={openQuests}
-          setOpenQuests={setOpenQuests}
-        />
-        <QuestListItem
-          id="2"
-          openQuests={openQuests}
-          setOpenQuests={setOpenQuests}
-        />
+        {quests?.map((quest) => (
+          <QuestListItem
+            key={quest.id}
+            quest={quest}
+            openQuests={openQuests}
+            setOpenQuests={setOpenQuests}
+          />
+        ))}
       </ul>
       <Modal
+        mutation={handleCreateQuest}
         buttonLabel="Criar Aventura"
         content={
-          <form className="flex flex-col gap-4">
-            <Input label="Nome da Aventura" name="questName" required />
-            <Input label="Data de Início" name="startDate" required />
-            <Input label="Objetivo" name="objective" required />
+          <form id="new-quest" className="flex flex-col gap-4">
+            <Input
+              label="Nome da Aventura"
+              name="questName"
+              required
+              maxLength={64}
+            />
+            <Input
+              label="Data de Início"
+              name="startDate"
+              required
+              type="date"
+            />
+            <Input
+              label="Próximo objetivo"
+              name="objective"
+              required
+              maxLength={64}
+            />
             <input
               name="activities"
               required
               hidden
+              maxLength={64}
               value={activities.join(",")}
             />
             <div id="activities">
@@ -67,6 +119,7 @@ export default function CampaignQuests() {
                 name=""
                 addNew
                 placeholder="Adicionar mais uma atividade"
+                maxLength={64}
                 activities={activities}
                 setActivities={setActivities}
               />
@@ -82,8 +135,9 @@ export default function CampaignQuests() {
                 )}
               </AnimatePresence>
             </div>
-            <Input label="Recompensa" name="reward" />
-            <Input label="Nível Recomendado" name="recommendedLevel" required />
+            <Input label="Recompensa" name="reward" maxLength={64} />
+            <Input label="Nível Recomendado" name="recommendedLevel" />
+            <TextArea label="Descrição" name="description" maxLength={1280} />
           </form>
         }
         title="Crie a sua aventura"
