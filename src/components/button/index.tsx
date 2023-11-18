9c6d97c742436f8d2e54f6114f13a8cb182925c9
Igor Stefano <igstefano90@live.com>
2023-11-18 18:15:20 -0300

    fix(button): corrige o visual do botão desabilitado

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -16,7 +16,7 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className="py-2- flex w-full items-center justify-center gap-4 rounded-3xl bg-burgundy p-2 text-base font-bold text-gray-50 drop-shadow-default transition-all duration-300 hover:bg-burgundy-900 focus:bg-burgundy-900 disabled:bg-gray-200 disabled:text-gray-900 disabled:opacity-80"
+      className="py-2- flex w-full items-center justify-center gap-4 rounded-3xl bg-burgundy p-2 text-base font-bold text-gray-50 drop-shadow-default transition-all duration-300 hover:bg-burgundy-900 focus:bg-burgundy-900 disabled:bg-gray-900 disabled:opacity-20"
       disabled={disabled}
       onClick={onClick}
     >
